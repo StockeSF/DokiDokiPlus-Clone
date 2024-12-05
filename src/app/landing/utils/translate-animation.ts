03d9fe41b501272ba83149873f46d0bebc20b3d5
Frank C. Fonseca <fonseca.fr2709@gmail.com>
2024-12-04 23:09:31 -0400

    Added breakpoints in translate-animation

--- src/app/landing/utils/translate-animation.ts
+++ src/app/landing/utils/translate-animation.ts
@@ -1,10 +1,18 @@
 type AnimationTypes = 'logo' | 'char-back' | 'char-front' | 'bg' | 'buynow'
+type BreakpointTypes = 'md' | 'lg' | 'xl' | '2xl' 
 
 export class TranslateAnimation {
   elements: NodeListOf<HTMLElement>
-  
+  matchMedia: Record<BreakpointTypes, MediaQueryList | null>
+
   constructor(elements: NodeListOf<HTMLElement>) {
     this.elements = elements
+    this.matchMedia = {
+      "2xl": null,
+      xl: null,
+      lg: null,
+      md: null
+    }
   }
 
   private addInitStyles(entry: HTMLElement) {
@@ -22,8 +30,35 @@ export class TranslateAnimation {
     }
   }
 
+  private createMatchMediaQuery(value: string | number) {
+    return window.matchMedia(`(min-width: ${value}px)`)
+  }
+
+  private getMediaQuery(query: BreakpointTypes) {
+    switch (query) {
+      case '2xl': return this.matchMedia[query] = this.createMatchMediaQuery(1536)
+      case 'xl': return this.matchMedia[query] = this.createMatchMediaQuery(1280)
+      case 'lg': return this.matchMedia[query] = this.createMatchMediaQuery(1024)
+      case 'md': return this.matchMedia[query] = this.createMatchMediaQuery(768)
+      default: {
+        console.log(`getMediaQuery Error: no existe la media query: ${query}`)
+        return null
+      }
+    }
+  }
+
   private getScrollValue(entry: HTMLElement) {
     let windowValue = window.scrollY == 0 ? 'unset' : `${window.scrollY}px`
+    let currentMediaQuery = entry.dataset.translateMediaQuery as BreakpointTypes | undefined
+
+    if (currentMediaQuery === undefined) return entry.style.setProperty('--_window-value', windowValue)
+
+    if (this.matchMedia[currentMediaQuery] === null) this.getMediaQuery(currentMediaQuery)
+
+    if (this.matchMedia[currentMediaQuery] !== null && !this.matchMedia[currentMediaQuery].matches) {
+      windowValue = 'unset'
+    }
+
     return entry.style.setProperty('--_window-value', windowValue)
   }
 
